45b4c0bd811497369215ed9201cbeff315eb183b
jadorks <adorkorj@gmail.com>
2020-01-21 17:15:08 +0000

    Completed input lines

---
+++ Shop.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Shop{
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the product name");
+        String name = input.nextLine();
+
+        System.out.println("Enter quantity of product");
+        int quantity = input.nextInt();
+
+        System.out.println("Enter price of product");
+        double price = input.nextDouble();
+
+        
+    }
+}
